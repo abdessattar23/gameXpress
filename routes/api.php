ff5827ac9e6f7e59528e278c3c847cdb7072dbc5
lahcen <lahcenwirghanec11@gmail.com>
2025-03-12 22:41:26 +0000

    dasbhboard stats endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\Api\V1\Admin\AdminController;
+use App\Http\Controllers\Api\V1\Admin\DashboardController;
 use App\Http\Controllers\Api\V1\Admin\ProductController;
 use App\Http\Controllers\Api\V1\Auth\AuthController;
 use Illuminate\Http\Request;
@@ -10,12 +11,13 @@ use \Illuminate\Auth\Middleware\Authorize;
 
 
 // Auth api routes
-Route::post('/register', [AuthController::class, 'Register'])->name('register');
-Route::post('/login', [AuthController::class, 'Login'])->name('login');
-Route::post('/logout', [AuthController::class, 'Logout'])->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // dashboard routes
-Route::get('/v1/admin/dashboard', [AdminController::class, 'index'])->middleware(['auth:sanctum', 'can:view_dashboard']);
+Route::get('/v1/admin/dashboard', [DashboardController::class, 'index'])->middleware('auth:sanctum');
+
 
 // product routes
 Route::apiResource('products', ProductController::class);
